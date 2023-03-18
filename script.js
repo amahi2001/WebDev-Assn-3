6f1dcb9ea8b115290605bd119aa5d85c35656239
Abrar <amahi2001@gmail.com>
2023-03-18 19:37:14 -0400

    ability color a cell on selected color by creating giving newly created cell an onClick event listener which triggers fillCellOnClick() in the addR() addC() methods. Other minor changes

--- script.js
+++ script.js
@@ -15,11 +15,16 @@ let tBody = document.getElementById("table_body");
 function addR() {
     // Create a new row at the end of the table
     let newRow = tBody.insertRow(-1);
-    // Create a new <td> for num cols - 1 in the row
+    //! Create a new <td> for numCols if numCols > 0
     for (let i = 0; i < numCols - 1; i++) {
         newRow.insertCell(-1);
+        //give the cell a onclick function to set the background color
+        fillCellOnClick(newRow.cells[i]);
     }
+    //!this section is for if numCols is 0
     newRow.insertCell(-1)
+    //give the cell a onclick function to set the background color
+    fillCellOnClick(newRow.cells[numCols]);
     // Increment the number of rows
     numRows++;
 }
@@ -29,6 +34,8 @@ function addC() {
     // Create a new <td> at end of row for each row in table
     for (const i of tBody.rows) {
         i.insertCell(-1);
+        //give the cell a onclick function to set the background color
+        fillCellOnClick(i.cells[numCols + 1]);
     }
     // Increment the number of columns
     numCols++;
@@ -58,6 +65,13 @@ colorPicker.addEventListener('change', () => {
     selectedColor = colorPicker.value;
     console.log(selectedColor); // Log the selected color value
 });
+
+//gives the cell a onclick function to set the background color
+function fillCellOnClick(cell) {
+    cell.onclick = function () {
+        cell.style.backgroundColor = selectedColor;
+    }
+}
 
 // Fill all uncolored cells
 function fillU() {
