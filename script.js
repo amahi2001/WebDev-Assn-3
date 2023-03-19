f74a30b83ffee773ce94d40a0379e54d38106fa5
Abrar <amahi2001@gmail.com>
2023-03-19 16:49:46 -0400

    fixes to addR and addC where some cells where not getting onClick() prop. Also cols were being created before rows were.

--- script.js
+++ script.js
@@ -15,30 +15,37 @@ let tBody = document.getElementById("table_body");
 function addR() {
     // Create a new row at the end of the table
     let newRow = tBody.insertRow(-1);
-    //! Create a new <td> for numCols if numCols > 0
-    for (let i = 0; i < numCols - 1; i++) {
-        newRow.insertCell(-1);
-        //give the cell a onclick function to set the background color
-        fillCellOnClick(newRow.cells[i]);
-    }
-    //!this section is for if numCols is 0
-    newRow.insertCell(-1)
-    //give the cell a onclick function to set the background color
-    fillCellOnClick(newRow.cells[numCols]);
     // Increment the number of rows
     numRows++;
+    //! Create a new <td> for numCols if numCols > 0
+    if (numCols > 0) {
+        for (let i = 0; i < numCols + 1; i++) {
+            newRow.insertCell(-1);
+            //give the cell a onclick function to set the background color
+            fillCellOnClick(newRow.cells[i]);
+        }
+    }
+    else {
+        // Create a new <td> at the end of the row
+        newRow.insertCell(-1);
+        //give the cell a onclick function to set the background color
+        fillCellOnClick(newRow.cells[0]);
+    }
 }
 
 // Add a column
 function addC() {
-    // Create a new <td> at end of row for each row in table
-    for (const i of tBody.rows) {
-        i.insertCell(-1);
-        //give the cell a onclick function to set the background color
-        fillCellOnClick(i.cells[numCols + 1]);
+    if (numRows > 0) {
+        // Create a new <td> at end of row for each row in table
+        for (const i of tBody.rows) {
+            i.insertCell(-1);
+            //give the cell a onclick function to set the background color
+            fillCellOnClick(i.cells[numCols + 1]);
+        }
+        // Increment the number of columns
+        numCols++;
     }
-    // Increment the number of columns
-    numCols++;
+
 }
 
 // Remove a row
@@ -63,7 +70,6 @@ function removeC() {
 colorPicker.addEventListener('change', () => {
     // Get the selected color value
     selectedColor = colorPicker.value;
-    console.log(selectedColor); // Log the selected color value
 });
 
 //gives the cell a onclick function to set the background color
